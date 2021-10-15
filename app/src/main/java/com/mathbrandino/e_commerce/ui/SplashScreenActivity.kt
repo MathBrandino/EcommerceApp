3a905d58aa154d7ea86e6d98eb63c74b9d93e278
Matheus Brandino <m.v.brandino@gmail.com>
2021-10-14 23:02:57 -0300

    create splash screen

---
+++ app/src/main/java/com/mathbrandino/e_commerce/ui/SplashScreenActivity.kt
@@ -0,0 +1,20 @@
+package com.mathbrandino.e_commerce.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.mathbrandino.e_commerce.MainActivity
+import com.mathbrandino.e_commerce.R
+
+class SplashScreenActivity : AppCompatActivity(R.layout.activity_splash_screen) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Handler(Looper.getMainLooper())
+            .postDelayed({
+                startActivity(Intent(this, MainActivity::class.java))
+            }, 2000)
+    }
+}

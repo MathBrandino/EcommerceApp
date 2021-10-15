14c4489fbbdaf46bf9cca227a96c3adf22ce5da9
Matheus Brandino <m.v.brandino@gmail.com>
2021-10-15 00:27:20 -0300

    image search implemented

--- app/src/main/java/com/mathbrandino/e_commerce/ui/ProductFormActivity.kt
+++ app/src/main/java/com/mathbrandino/e_commerce/ui/ProductFormActivity.kt
@@ -1,7 +1,10 @@
 package com.mathbrandino.e_commerce.ui
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import coil.load
+import coil.size.Scale
 import com.mathbrandino.e_commerce.databinding.ActivityProductFormBinding
 
 class ProductFormActivity : AppCompatActivity() {
@@ -13,6 +16,30 @@ class ProductFormActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.productFormImageUrl.editText?.apply {
+            setOnEditorActionListener { _, _, _ ->
+                searchImage(text.toString())
+                true
+            }
+        }
+        binding.productFormImageSearch.setOnClickListener {
+            searchImage(binding.productFormImageUrl.editText?.text.toString())
+        }
     }
 
+    private fun searchImage(link: String) {
+        binding.productFormImage.load(link) {
+            scale(Scale.FIT)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        android.R.id.home -> {
+            finish()
+            true
+        }
+        else -> true
+    }
 }

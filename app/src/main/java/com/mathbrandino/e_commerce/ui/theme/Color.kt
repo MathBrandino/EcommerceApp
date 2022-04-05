4ed9dd57bba07c7207479d778d7783b433b4e20c
Matheus Brandino <m.v.brandino@gmail.com>
2022-04-05 20:48:49 -0300

    create base config for compose

---
+++ app/src/main/java/com/mathbrandino/e_commerce/ui/theme/Color.kt
@@ -0,0 +1,19 @@
+package com.mathbrandino.e_commerce.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val Orange700 = Color(0xFFE64A19)
+val Orange200 = Color(0xFFFF9800)
+val Blue700 = Color(0xFF0288D1)
+val Blue200 = Color(0xFF81D4FA)
+val Black = Color(0xFF000000)
+val Black100 = Color(0xFF3C3C3C)
+val Gray = Color(0xFFC5C3C3)
+val DarkBlue700 = Color(0xFF1976D2)
+val White = Color(0xFFFFFFFF)
+

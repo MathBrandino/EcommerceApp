c1e3eec77a0d886a30fce031ece119d7fa1cf7c2
Matheus Brandino <m.v.brandino@gmail.com>
2022-04-11 23:14:32 -0300

    make cart 'observable'

--- app/src/main/java/com/mathbrandino/e_commerce/domain/model/Cart.kt
+++ app/src/main/java/com/mathbrandino/e_commerce/domain/model/Cart.kt
@@ -1,15 +1,19 @@
 package com.mathbrandino.e_commerce.domain.model
 
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import com.mathbrandino.e_commerce.data.local.product.Product
+import java.math.BigDecimal
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class Cart @Inject constructor() {
 
-    val items = ArrayList<CartItem>()
+    var items = mutableStateListOf<CartItem>()
 
-    fun getTotal() = items.sumOf { it.getTotal() }
+    var total = mutableStateOf(BigDecimal.ZERO)
+        private set
 
     fun add(product: Product) {
         val item = items.find { it.product == product }
@@ -17,16 +21,19 @@ class Cart @Inject constructor() {
             items.add(CartItem(product, 1))
         else
             increaseQuantityOf(product)
+        atualiza()
     }
 
     fun increaseQuantityOf(product: Product) {
         val item = items.find { it.product == product }
         item?.quantity = item?.quantity?.plus(1)!!
+        atualiza()
     }
 
     fun decreaseQuantityOf(product: Product): Boolean {
         val item = items.find { it.product == product }
         item?.quantity = item?.quantity?.minus(1)!!
+        atualiza()
         if (item.quantity == 0) {
             return items.remove(items.find { it.product == product })
         }
@@ -35,5 +42,10 @@ class Cart @Inject constructor() {
 
     fun clear() {
         items.clear()
+        atualiza()
+    }
+
+    private fun atualiza() {
+        total.value = items.sumOf { it.getTotal() }
     }
 }
